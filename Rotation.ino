29bc8f7391cc510e98b4accb0f11c60c6d562075
Ignat Borodikhin <ignat@ngs.ru>
2019-03-12 23:46:50 +0700

    Add files via upload

--- Rotation.ino
+++ Rotation.ino
@@ -7,15 +7,15 @@ OneWire oneWire(12);
 DallasTemperature sensors(&oneWire);
 float temp;
 
-int fanPinOne = 9;
-int fanPinTwo = 10;
-int fanPinPWM = 11;
+int fanPinOne = 8;
+int fanPinTwo = 7;
+int fanPinPWM = 6;
 
-int monitorMotorPinOne = 8;
-int monitorMotorPinTwo = 7;
-int monitorMotorPinPWM = 6;
+int monitorMotorPinOne = 9;
+int monitorMotorPinTwo = 10;
+int monitorMotorPinPWM = 11;
 
-int endstopWidePosition = 150; // the value need to be experimentally defined
+int endstopWidePosition = 300; // the value need to be experimentally defined
 /*  
  *  We have two plywood gears with 10 and 50 teeth (1:5 reduction ratio).
  *  We need to rotate monitor wich attached to 50-tooth gear on 90 degrees,
@@ -26,13 +26,14 @@ int endstopWidePosition = 150; // the value need to be experimentally defined
  *  530 * tan (1/30 deg) = 0.3 mm or 1.5 mm per 5 impulses (or half from center)
  *  Let use 5 impulses as rotation quantity.
  */
-int wideTallPosition = 2750;
+int wideTallPosition = 10500;
 
 int CWendStopPin = 4; // CW rotation endstop button pin
 int CCWendStopPin = 5; // CCW rotation endstop button pin
 
 int CWendStopState; // CW rotation endstop state
 int CCWendStopState; // CCW rotation endstop state
+int incomingByte; // a variable to read incoming serial data into
 
 int direction = 1; // rotation direction flag: 1 means CW; -1  means CCW
 
@@ -54,7 +55,7 @@ void setup() {
   pinMode(CWendStopPin, INPUT);
   pinMode(CCWendStopPin, INPUT);
   
-  rotate(direction, 5000, 125); /* 5000 is a big number because we need to
+  rotate(direction, 15000, 255); /* 5000 is a big number because we need to
   rotate until CW rotation endstop turns 1 to define starting position */
 
   direction = direction * (-1);
@@ -62,24 +63,32 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) {
-    if (Serial.read() == "o") {
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    if (incomingByte == 'o') {
+      Serial.print("o is OK!");
+      Serial.println();
       rotate(direction, wideTallPosition, 255);
-      Serial.print("Rotation comlete!");
+      Serial.print("Rotation comlete! ");
       Serial.print(newPosition);
       Serial.println();
+      direction = direction * (-1);
     }
-    direction = direction * (-1);
   }
 
   sensors.requestTemperatures();  // Send the command to get temperature
   temp = sensors.getTempCByIndex(0); // Read temperature from the first sensor
-  digitalWrite(fanPinOne, LOW);
-  digitalWrite(fanPinTwo, HIGH);
+  digitalWrite(fanPinOne, HIGH);
+  digitalWrite(fanPinTwo, LOW);
   /* If temperature is 18 or low the fan is off. 
    * While temperature rising to 40 degrees PWM rising to 255
    */
-  analogWrite(fanPinPWM, constrain(map(temp, 18, 40, 0, 255) ,0, 255));
+  Serial.print("Temperature is: ");
+  Serial.print(temp);
+  Serial.println();
+
+  //analogWrite(fanPinPWM, constrain(map(temp, 30, 40, 0, 255) ,0, 255));
 }
 
 void rotate(int dir, int steps, int PWM) {
@@ -91,22 +100,40 @@ void rotate(int dir, int steps, int PWM) {
   startPosition = newPosition;
   if (dir == 1) {
     // CW rotation
-    digitalWrite(monitorMotorPinOne, LOW);
-    digitalWrite(monitorMotorPinTwo, HIGH);
+    digitalWrite(monitorMotorPinOne, HIGH);
+    digitalWrite(monitorMotorPinTwo, LOW);
     stopPin = CWendStopPin;
   } else {
     // CCW rotation
-    digitalWrite(monitorMotorPinOne, HIGH);
-    digitalWrite(monitorMotorPinTwo, LOW);
+    digitalWrite(monitorMotorPinOne, LOW);
+    digitalWrite(monitorMotorPinTwo, HIGH);
     stopPin = CCWendStopPin;
   }
   timer = millis();
-  while ((newPosition - startPosition < dir * steps) && (digitalRead(stopPin) != HIGH)) {
+
+  Serial.print(newPosition - startPosition);
+  Serial.print(" ");
+  Serial.print(dir * steps);
+
+  Serial.println();
+
+  while ((abs(newPosition - startPosition) < steps) && (digitalRead(stopPin) != HIGH)) {
     analogWrite(monitorMotorPinPWM, PWM);
     newPosition= motorEnc.read();
     if (newPosition != oldPosition) {
       oldPosition = newPosition;
     }
+  Serial.print(newPosition);
+  Serial.print(" ");
+  Serial.print(startPosition);
+  Serial.print(" ");
+  Serial.print(dir);
+  Serial.print(" ");
+  Serial.print(steps);
+  Serial.print(" ");
+  Serial.println();
+
+    
   }
   analogWrite(monitorMotorPinPWM, 0);
   Serial.print("Cutoff position = ");
